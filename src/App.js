80fd1e694e5ed85db445f7819ae129d1c9e3e1c4
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-07-08 18:32:14 +0500

    p

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React bo'yicha 1-loyiha.
-        </p>
-        <p>Bugun 08.07.2021 sanada react darsimizani boshladik.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="shadow">
+        <div className="container d-flex align-items-center">
+          <img src={logo} className="" alt="logo" />
+          <p>
+            React App
+          </p>
+          <p>Bugun 08.07.2021 sanada react darsimizani boshladik.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
       </header>
     </div>
   );
